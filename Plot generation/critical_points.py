dadec471e3329c6bbc0a63155b604196aaff12f1
Benoît Richard <kolaru@hotmail.com>
2018-10-09 01:35:41 +0200

    Finalise critical point plots

--- Plot generation/critical_points.py
+++ Plot generation/critical_points.py
@@ -2,9 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 plt.rc("text", usetex=True)
-
-continuous = False
-discontinuous = True
+plt.rc("font", size=10, family="palatino linotype")
 
 y_shift = 0.05
 umin = 0.6
@@ -20,65 +18,80 @@ y = np.zeros_like(x)
 y[x > 1] = np.sqrt(x_high - (1 - y_shift)) - np.sqrt(y_shift)
 y = 1 - y
 
+ymin = np.min(y) - 0.1*np.abs(np.min(y))
+ymax = 1.3
+
 z_low = np.ones_like(x_low)
 z_high = 1 - (np.sqrt(x_high - (1 - y_shift)) + 0.05)
 
-if continuous:
-    fig, ax = plt.subplots()
-    base_curve, = ax.plot(x, np.ones_like(x), color="gray", linestyle="dotted")
-    ax.plot(x, y, color="gray", linestyle="dotted")
-    trivial_curve, = ax.plot(x[umask(x)], np.ones_like(x[umask(x)]), linewidth=3)
-    nontrivial_curve, = ax.plot(x[umask(x)], y[umask(x)], linewidth=3, linestyle="dashed")
-    ax.plot(u, 1.1*np.ones_like(u), color="black", linewidth=2)
-    ax.plot([umin, umin], [1.1 - capwidth, 1.1 + capwidth], color="black", linewidth=2)
-    ax.plot([umax, umax], [1.1 - capwidth, 1.1 + capwidth], color="black", linewidth=2)
-    ax.plot(1, 1, color="black", marker="o", markersize="8", mec="white", mew="2")
+fig, axes = plt.subplots(1, 2, figsize=(6, 3))
 
-    ax.text(1, 1.15, "$U$")
-    ax.annotate('Critical point $(\lambda^*, u_T)$', xy=(1, 1), xytext=(0.8, 0.8),
-                arrowprops=dict(facecolor='black', shrink=0.1),
-                ha="right"
-                )
+ax = axes[0]
 
-    ax.set_ylim(np.min(y) - 0.1*np.abs(np.min(y)), 1.5)
-    ax.set_xlabel("$\lambda$")
-    ax.set_ylabel("$u$")
-    ax.legend([base_curve, trivial_curve, nontrivial_curve],
-              ["All solutions", "Trivial inverse function $h_T(\lambda)$", "Nontrivial inverse function $h(\lambda)$"],
-              loc="lower left")
+base_curve, = ax.plot(x, np.ones_like(x), color="gray", linestyle="dotted")
+ax.plot(x, y, color="gray", linestyle="dotted")
+trivial_curve, = ax.plot(x[umask(x)], np.ones_like(x[umask(x)]), linewidth=3)
+nontrivial_curve, = ax.plot(x[umask(x)], y[umask(x)], linewidth=3, linestyle="dashed")
+ax.plot(u, 1.1*np.ones_like(u), color="black", linewidth=2)
+ax.plot([umin, umin], [1.1 - capwidth, 1.1 + capwidth], color="black", linewidth=2)
+ax.plot([umax, umax], [1.1 - capwidth, 1.1 + capwidth], color="black", linewidth=2)
+ax.plot(1, 1, color="black", marker="o", markersize="8", mec="white", mew="2")
 
-if discontinuous:
-    fig, ax = plt.subplots()
+ax.text(1, 1.15, "$U$")
 
-    base_curve, = ax.plot(x, np.ones_like(x), color="gray", linestyle="dotted")
-    ax.plot(x_low, z_low, color="gray", linestyle="dotted")
-    ax.plot(x_high, z_high, color="gray", linestyle="dotted")
+labelpos = (0.6, 0.5)
+ax.annotate('', xy=(1, 1), xytext=labelpos,
+            arrowprops=dict(facecolor='black', shrink=0.1),
+            ha="center"
+            )
+ax.text(labelpos[0], labelpos[1] - 0.05, 'Critical point $(\lambda^*, u_T)$',
+        ha="center")
 
-    trivial_curve, = ax.plot(x[umask(x)], np.ones_like(x[umask(x)]), linewidth=3)
-    nontrivial_curve, = ax.plot(x_low[umask(x_low)], z_low[umask(x_low)], color="C1", linewidth=3, linestyle="dashed")
-    ax.plot(x_high[umask(x_high)], z_high[umask(x_high)], color="C1", linewidth=3, linestyle="dashed")
-    ax.plot(u, 1.1*np.ones_like(u), color="black", linewidth=2)
-    ax.plot([umin, umin], [1.1 - capwidth, 1.1 + capwidth], color="black", linewidth=2)
-    ax.plot([umax, umax], [1.1 - capwidth, 1.1 + capwidth], color="black", linewidth=2)
-    ax.plot(1, 1, color="black", marker="o", markersize="8", mec="white", mew="2")
-    ax.plot(1, np.max(z_high), color="black", marker="o", markersize="8", mec="white", mew="2")
+ax.set_ylim(ymin, ymax)
+ax.set_xlabel("$\lambda$")
+ax.set_ylabel("$u$")
+ax.set_xticks([])
+ax.set_yticks([])
 
-    ax.text(1, 1.15, "$U$")
-    ax.annotate('Critical point $(\lambda^*, u_T)$', xy=(1, 1), xytext=(0.8, 0.8),
-                arrowprops=dict(facecolor='black', shrink=0.1),
-                ha="right"
-                )
+ax = axes[1]
 
-    ax.annotate('Critical point $(\lambda^*, u^\dagger)$', xy=(1, np.max(z_high)), xytext=(1.3, 0.8),
-                arrowprops=dict(facecolor='black', shrink=0.1),
-                ha="left"
-                )
+base_curve, = ax.plot(x, np.ones_like(x), color="gray", linestyle="dotted")
+ax.plot(x_low, z_low, color="gray", linestyle="dotted")
+ax.plot(x_high, z_high, color="gray", linestyle="dotted")
 
-    ax.set_ylim(np.min(y) - 0.1*np.abs(np.min(y)), 1.5)
-    ax.set_xlabel("$\lambda$")
-    ax.set_ylabel("$u$")
-    ax.legend([base_curve, trivial_curve, nontrivial_curve],
-              ["All solutions", "Trivial inverse function $h_T(\lambda)$", "Nontrivial inverse function $h(\lambda)$"],
-              loc="lower left")
+trivial_curve, = ax.plot(x[umask(x)], np.ones_like(x[umask(x)]), linewidth=3)
+nontrivial_curve, = ax.plot(x_low[umask(x_low)], z_low[umask(x_low)], color="C1", linewidth=3, linestyle="dashed")
+ax.plot(x_high[umask(x_high)], z_high[umask(x_high)], color="C1", linewidth=3, linestyle="dashed")
+ax.plot(u, 1.1*np.ones_like(u), color="black", linewidth=2)
+ax.plot([umin, umin], [1.1 - capwidth, 1.1 + capwidth], color="black", linewidth=2)
+ax.plot([umax, umax], [1.1 - capwidth, 1.1 + capwidth], color="black", linewidth=2)
+ax.plot(1, 1, color="black", marker="o", markersize="8", mec="white", mew="2")
+ax.plot(1, np.max(z_high), color="black", marker="o", markersize="8", mec="white", mew="2")
 
+ax.text(1, 1.15, "$U$")
+
+labelpos = (1.6, 0.6)
+ax.annotate('Critical point\n $(\lambda^*, u_T)$', xy=(1, 1), xytext=labelpos,
+            arrowprops=dict(facecolor='black', shrink=0.15),
+            ha="center"
+            )
+
+labelpos = (0.6, 0.4)
+ax.annotate('', xy=(1, np.max(z_high)), xytext=labelpos,
+            arrowprops=dict(facecolor='black', shrink=0.1),
+            ha="center"
+            )
+ax.text(labelpos[0], labelpos[1] - 0.05, 'Critical point $(\lambda^*, u^\dagger)$',
+        ha="center")
+
+ax.set_ylim(ymin, ymax)
+ax.set_xlabel("$\lambda$")
+ax.set_xticks([])
+ax.set_yticks([])
+# ax.legend([base_curve, trivial_curve, nontrivial_curve],
+#           ["All solutions", "Trivial inverse function $h_T(\lambda)$", "Nontrivial inverse function $h(\lambda)$"],
+#           loc="lower left")
+
+fig.tight_layout()
+fig.savefig("LaTeX/Report/critical_point.pdf")
 plt.show()
