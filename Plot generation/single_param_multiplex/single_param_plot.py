5a07ca259735825fb643799a00e8d7cfd2d68b28
Benoît Richard <kolaru@hotmail.com>
2018-10-09 03:46:56 +0200

    Finalize ER single param plot

--- Plot generation/single_param_multiplex/single_param_plot.py
+++ Plot generation/single_param_multiplex/single_param_plot.py
@@ -39,19 +39,25 @@ def plot_patches(ax, name):
     ax.add_collection(PatchCollection(unkown, facecolor="C1"))
     ax.set_xlim(xmin, xmax)
 
-def plot_single_param(name, n_layers):
+def plot_single_param(name, n_layers, labelpos=None, legpos="upper left"):
     fig, ax = plt.subplots(1, 1, sharex=True, figsize=figsize)
-    for L in n_layers:
+    for k, L in enumerate(n_layers):
         fullname = "{}{}.json".format(name, L)
         plot_patches(ax, fullname)
+        if labelpos is not None:
+            x, y = labelpos[k]
+            ax.text(x, y, "$L = {}$".format(L))
+                    # bbox=dict(facecolor="white", alpha=0.5, linewidth=0))
 
     ax.legend((Rectangle((0, 0), 1, 1, facecolor="C0"),
                 Rectangle((0, 0), 1, 1, facecolor="C1")),
-                ("Solutions exist", "Unkown status"), loc="upper left")
+                ("Solutions exist", "Unkown status"), loc=legpos)
     ax.set_xlabel("$c$")
     ax.set_ylabel("$u$")
     fig.tight_layout()
     fig.savefig("LaTeX/Report/multilayer_single_param_{}.pdf".format(name))
 
-plot_single_param("ErdosRenyiGraph", [2, 3, 4, 5])
+plot_single_param("ErdosRenyiGraph",
+                  [2, 3, 4, 5],
+                  labelpos=[(2.25, 0.6), (2.99, 0.55), (3.44, 0.5), (3.65, 0.4)])
 plt.show()
