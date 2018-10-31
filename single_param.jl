d2becfe4bf915410a0fe0765946a6adbabfea351
Benoît Richard <kolaru@hotmail.com>
2018-10-31 20:17:36 +0100

    Clean up single param code

--- single_param.jl
+++ single_param.jl
@@ -8,26 +8,23 @@ using GeneratingFunctions
 using Graphs
 using Utils
 
-function ufunc_single_param(z, layer, param, L)
-    return 1. - (1. - g1(layer, z, param))*(1 - g0(layer, z, param))^(L - 1)
+"""
+    Fixpoint function `ψ`.
+
+Order of arguments `λ` and `z` are inverted compare to what is done in the
+report, to match the definition of `g0` and `g1` in `GeneratingFunctions.jl`.
+"""
+function ψ(graphtype, z, λ, L)
+    return 1. - (1. - g1(graphtype, z, λ))*(1 - g0(graphtype, z, λ))^(L - 1)
 end
 
-function dufunc_single_param(z, layer, param, L)
-    p0 = 1 - g0(layer, z, param)
-    return ( dg1(layer, z, param)*p0 + (L - 1)*(1 - g1(layer, z, param))*dg0(layer, z, param) ) * p0^(L-2)
+function dψ(graphtype, z, λ, L)
+    p0 = 1 - g0(graphtype, z, λ)
+    return ( dg1(graphtype, z, λ)*p0 + (L - 1)*(1 - g1(graphtype, z, λ))*dg0(graphtype, z, λ) ) * p0^(L-2)
 end
 
-function ures_single_param(z, layer, param, L)
-    return ufunc_single_param(z, layer, param, L) - z
-end
-
-function dures_single_param(z, layer, param, L)
-    return dufunc_single_param(z, layer, param, L) - 1
-end
-
-
-function fixpoint_refine(X, layer, Λ, L)
-    FX = ufunc_single_param(X, layer, Λ, L)
+function fixpoint_refine(graphtype, X, Λ, L)
+    FX = ψ(graphtype, X, Λ, L)
     status = :unkown
     if FX ⊆ X
         satus = :exist
@@ -37,14 +34,30 @@ function fixpoint_refine(X, layer, Λ, L)
     return Root(NX × Λ, status)
 end
 
-function generate_single_param_data(layer, pbound, L=2, tol=0.005, C=Krawczyk)
-    working = [UNITINTERVAL × pbound]
+"""
+    Residual function `f_λ`.
+
+`f_λ(z) == 0` iff `ψ(z, λ) = z`.
+"""
+struct ResFunc{GT <: GraphType}
+    graphtype::Type{GT}
+    λ
+    L::Int
+end
+
+func(rf::ResFunc) = z -> ψ(rf.graphtype, z, rf.λ, rf.L) - z
+deriv(rf::ResFunc) = z -> dψ(rf.graphtype, z, rf.λ, rf.L) - 1
+
+
+function generate_single_param_data(graphtype, Λ0, L=2, tol=0.005)
+    working = [UNITINTERVAL × Λ0]
     stored = []
 
     while !isempty(working)
         X, Λ = popfirst!(working)
-        f(z) = ures_single_param(z, layer, Λ, L)
-        df(z) = dures_single_param(z, layer, Λ, L)
+        resfunc = ResFunc(graphtype, Λ, L)
+        f = func(resfunc)
+        df = deriv(resfunc)
 
         rts = roots(f, df, X, Krawczyk, tol)
 
@@ -54,7 +67,7 @@ function generate_single_param_data(layer, pbound, L=2, tol=0.005, C=Krawczyk)
                 push!(stored, Root(Y × Λ, :unique))
             else
                 if diam(Y × Λ) < tol
-                    refined = fixpoint_refine(Y, layer, Λ, L)
+                    refined = fixpoint_refine(graphtype, Y, Λ, L)
                     !isempty(refined.interval) && push!(stored, refined)
                 else
                     append!(working, bisect(Y × Λ))
@@ -63,30 +76,27 @@ function generate_single_param_data(layer, pbound, L=2, tol=0.005, C=Krawczyk)
         end
     end
 
-    open("Plot generation/single_param_multiplex/$layer$L.json", "w") do file
+    open("Plot generation/single_param_multiplex/$graphtype$L.json", "w") do file
         JSON.print(file, stored)
     end
 
     return stored
 end
 
-function single_param_ER(pbound=2..4, Ls=[2, 3, 4, 5], tol=0.02)
+function single_param_ER(Λ0=2..4, Ls=[2, 3, 4, 5], tol=0.02)
     for L in Ls
-        generate_single_param_data(ErdosRenyiGraph, pbound, L, tol)
+        generate_single_param_data(ErdosRenyiGraph, Λ0, L, tol)
     end
 end
 
-function single_param_geometric(pbound=2..5, Ls=[2, 3, 4], tol=0.02)
+function single_param_geometric(Λ0=2..5, Ls=[2, 3, 4], tol=0.02)
     for L in Ls
-        generate_single_param_data(GeometricGraph, pbound, L, tol)
+        generate_single_param_data(GeometricGraph, Λ0, L, tol)
     end
 end
 
-function single_param_SF(pbound=1.5..2.4, Ls=[2, 3, 4], tol=0.005)
+function single_param_SF(Λ0=1.5..2.4, Ls=[2, 3, 4], tol=0.005)
     for L in Ls
-        println("L = $L")
-        regions = generate_single_param_data(ScaleFreeGraph, pbound, L, tol)
-        println(length(regions))
-        println()
+        generate_single_param_data(ScaleFreeGraph, Λ0, L, tol)
     end
 end
